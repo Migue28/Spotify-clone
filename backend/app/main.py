dd09f3b62a602207201f3dfc94a9ee330e6ab6d2
Eddy Cedeño <miguepoke28@gmail.com>
2021-08-11 18:15:38 -0500

    Add Login router to main Create login and refresh routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers.songs import router as songs_router
+from app.routers.login import router as login_router
 
 origins = ["http://localhost:8080",]
 
 def get_app() -> FastAPI:
     app = FastAPI()
     app.include_router(songs_router)
+    app.include_router(login_router)
     app.add_middleware(
         CORSMiddleware,
         allow_origins=origins,
--- backend/app/routers/login.py
+++ backend/app/routers/login.py
@@ -1,18 +1,52 @@
 import spotipy
 import os
 from spotipy.oauth2 import SpotifyOAuth
+from fastapi import APIRouter, HTTPException
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    client_id= os.environ.get('CLIENT_ID'),
-    client_secret= os.environ.get('CLIENT_SECRET'),
-    redirect_uri = os.environ.get('SPOTIPY_REDIRECT_URI'),
-    scope= os.environ.get('SCOPE'),
-))
+router = APIRouter()
 
-results = sp.current_user_saved_tracks()
-for idx, item in enumerate(results['items']):
-    track = item['track']
-    print(idx, track['artists'][0]['name'], " – ", track['name'])
+@router.post("/login")
+async def login(): 
+    try:
+        spauth = SpotifyOAuth(
+            client_id= os.environ.get('SPOTIPY_CLIENT_ID'),
+            client_secret= os.environ.get('SPOTIPY_CLIENT_SECRET'),
+            redirect_uri = os.environ.get('SPOTIPY_REDIRECT_URI'),
+            scope= os.environ.get('SPOTIPY_SCOPE'),
+        )
+        spotipy.Spotify(auth_manager=spauth)
+        cached_token = spauth.get_cached_token()
+        res = {
+            "cached_token": cached_token,
+            "refresh_token": cached_token["refresh_token"], 
+            "expires_at": cached_token["expires_at"],
+        }
+    except HTTPException:
+        raise HTTPException(status_code=404)
+    return json.dumps(res)
+
+@router.post("/refresh")
+async def refresh():
+    try:
+        spauth = SpotifyOAuth(
+            client_id= os.environ.get('SPOTIPY_CLIENT_ID'),
+            client_secret= os.environ.get('SPOTIPY_CLIENT_SECRET'),
+            redirect_uri = os.environ.get('SPOTIPY_REDIRECT_URI'),
+            scope= os.environ.get('SPOTIPY_SCOPE'),
+        )
+        cached_token = spauth.get_cached_token()
+        spauth.refresh_access_token(cached_token["refresh_token"])
+        cached_token = spauth.get_cached_token()
+        spotipy.Spotify(auth_manager=spauth)
+        res = {
+            "cached_token": cached_token,
+            "refresh_token": cached_token["refresh_token"], 
+            "expires_at": cached_token["expires_at"],
+        }
+    except HTTPException:
+        raise HTTPException(status_code=404)
+    return json.dumps(res)
